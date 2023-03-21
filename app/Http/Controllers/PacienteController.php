cbb41238308ebe03bee53e05ae00f71f225e16e7
AlysonDev <devalyson@gmail.com>
2023-03-21 20:49:12 -0300

    Resolvido o bug da imagem :tada:

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PacienteRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+use Carbon\Carbon;
+
+use App\Http\Requests\PacienteRequest;
+
 
 use App\Models\Paciente;
 
@@ -18,18 +25,26 @@ class PacienteController extends Controller
         return response()->json($pacientes);
     }
 
-    public function cadastrar(PacienteRequest $request)
+    public function cadastrar(PacienteRequest $req)
     {
-        $data = $request->validated();
+        $data = $req->validated();
 
 
-        if ($request->hasFile('foto') && $request->file('foto')->isValid()) {
-            $file = $request->file('foto');
+        if ($req->hasFile('foto') && $req->file('foto')->isValid()) {
+            $file = $req->file('foto');
             $extension = $file->extension();
             $filename = Str::uuid() . '.' . $extension;
             $path = $file->storeAs('public/fotos', $filename);
             $data['foto'] = Storage::url($path);
         }
+
+        // if ($req->hasFile('foto') && $req->file('foto')->isValid()) {
+        //     $file = $req->file('foto');
+        //     $extension = $file->extension();
+        //     $filename = Str::uuid() . '.' . $extension;
+        //     $path = $file->storeAs('public/fotos', $filename);
+        //     $data['foto'] = Storage::url($path);
+        // }
 
         $paciente = Paciente::create($data);
 
@@ -39,19 +54,51 @@ class PacienteController extends Controller
 
     public function insert(Request $req)
     {
-
-        $paciente = new Paciente;
-
-        $paciente->nome = $req->name;
-        $paciente->dt_nascimento = "1988-01-19";
-        $paciente->cpf = "02751811388";
-        $paciente->telefone = "85992188113";
-        $paciente->foto = "nulo";
-
-        $paciente->save();
+        // return response()->json($req, 201);
 
 
-        // return redirect('/listar');return response()->json($paciente, 201);
-        return response()->json($req["nome"], 201);
+        try {
+            $validator = Validator::make($req->all(), [
+                'nome' => 'required|string|max:255',
+                'dt_nascimento' => 'required|date',
+                'cpf' => 'required|string|max:255',
+                'telefone' => 'required|string|max:255',
+                // 'foto' => 'required|file',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+
+            $paciente = new Paciente;
+
+            $paciente->nome = $req->nome;
+            $paciente->dt_nascimento = Carbon::parse($req->dt_nascimento)->toDateString();
+            $paciente->cpf = $req->cpf;
+            $paciente->telefone = $req->telefone;
+
+            if ($req->hasFile('foto')) {
+                $imagem = $req->file('foto');
+                // Gerar um cpf para o nome do arquivo
+                $nomeImagem = $paciente->cpf . '.' . $imagem->getClientOriginalExtension();
+
+                Storage::putFileAs('/', $imagem, $nomeImagem);
+
+                $imagem->move(public_path('imagens'), $nomeImagem);
+                $paciente->foto = $nomeImagem;
+            } else {
+                $paciente->foto = "nulo";
+            }
+
+            // dd($paciente);
+            $paciente->save();
+
+            // $apenas = $paciente->id;
+
+            return response()->json($req, 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
