0ab2f9469dd2c0b4ef9cc85622aa16053e6e5920
AlysonDev <devalyson@gmail.com>
2023-03-29 21:58:42 -0300

    Campos na tabela

--- database/migrations/2023_03_30_005441_create_paciente_atendimentos_table.php
+++ database/migrations/2023_03_30_005441_create_paciente_atendimentos_table.php
@@ -12,8 +12,35 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('paciente_atendimentos', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary('id');
+            $table->uuid('paciente_id')->index('paciente_id');
             $table->timestamps();
+            $table->string('status')->default('Não atendido');
+            $table->double('t', 5, 2);
+            $table->integer('pas');
+            $table->integer('pad');
+            $table->integer('fc');
+            $table->integer('fr');
+
+            $table->boolean('febre')->default(false);
+            $table->boolean('coriza')->default(false);
+            $table->boolean('nariz_intupido')->default(false);
+            $table->boolean('cansaco')->default(false);
+            $table->boolean('tosse')->default(false);
+            $table->boolean('dor_cabeca')->default(false);
+            $table->boolean('dores_corpo')->default(false);
+            $table->boolean('mal_estar_geral')->default(false);
+            $table->boolean('dor_garganta')->default(false);
+            $table->boolean('dificuldade_respirar')->default(false);
+            $table->boolean('falta_paladar')->default(false);
+            $table->boolean('falta_olfato')->default(false);
+            $table->boolean('dificuldade_locomocao')->default(false);
+            $table->boolean('diarreia')->default(false);
+
+            $table->foreign('paciente_id')
+                ->references('id')
+                ->on('pacientes')
+                ->onDelete('cascade');
         });
     }
 
