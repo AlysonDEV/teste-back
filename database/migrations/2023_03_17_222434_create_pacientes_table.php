8a63b849e7a481f031f70619f1b771a18b915bf6
AlysonDev <devalyson@gmail.com>
2023-03-29 22:02:36 -0300

    Corrigindo o ID para tabela Pacientes

--- database/migrations/2023_03_17_222434_create_pacientes_table.php
+++ database/migrations/2023_03_17_222434_create_pacientes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pacientes', function (Blueprint $table) {
-            $table->uuid("id");
+            $table->uuid("id")->primary();
             $table->timestamps();
             $table->string("nome");
             $table->date("dt_nascimento");
